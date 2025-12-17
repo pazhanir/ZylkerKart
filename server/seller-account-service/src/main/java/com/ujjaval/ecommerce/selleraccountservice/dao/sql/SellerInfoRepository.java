b462f72a5ce86d9b3ae798467d780def51b8de95
Pazhani Ragunathan <pazhani.rph@gmail.com>
2025-12-17 16:15:23 +0530

    Removec unused service

--- server/seller-account-service/src/main/java/com/ujjaval/ecommerce/selleraccountservice/dao/sql/SellerInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ujjaval.ecommerce.selleraccountservice.dao.sql;
-
-import com.ujjaval.ecommerce.selleraccountservice.entity.sql.SellerInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SellerInfoRepository extends JpaRepository<SellerInfo, Integer> {
-}
